4521d935f11849c275081486cd9122828c359399
staceyl2 <staceyl2@illinois.edu>
2023-11-28 19:02:48 -0600

    stored procedure draft

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # Team055-TheLokis
-This is a template for CS411 project repository. Please make sure that your title follows the convention: [TeamID]-[YourTeamName]. All TeamID should have a three digit coding (i.e. if you are team 20, you should have `team020` as your ID.). You should also make sure that your url for this repository is [fa23-cs411-team000-teamname.git] so TAs can correctly clone your repository and keep them up-to-date.
+FlightDelayPro is a web-based application designed to help travelers with choosing the
+fastest and most time efficient flights based on past flight delays and cancellations. It
+helps them make informed decisions and reduce travel-related stress. With our
+comprehensive database and user-friendly interface, we will be able to offer users
+valuable insights into booking the perfect flight.
 
-Once you setup your project, please remember to submit your team formation to the team form.
-
-## Permission
-You should make sure you allow TAs to access your repository. You can add TA(s) as a collaborator to your repository.
-
-## Preparing for your release
-Eash submission should be in it's own [release](https://docs.github.com/en/repositories/releasing-projects-on-github/about-releases). Release are specific freezes to your repository. You should submit your commit hash on canvas or google sheet. When tagging your stage, please use the tag `stage.x` where x is the number to represent the stage.
-
-## Keeping things up-to-date
-You should make sure you keep your project root files up-to-date. Information for each file/folders are explained.
-
-## Code Contribution
-Individual code contribution will be used to evaluate individual contribution to the project.
+## Progress Report
+11/19: Backend mostly complete. Added trigger. 
+11/27: Tried writting the stored procedure requirement. 
+11/28: Stored procedure numbers add up/makes more sense. Still need double check if produce correct result. 
--- doc/stored_procedure.sql
+++ doc/stored_procedure.sql
@@ -1,32 +1,78 @@
+-- For SEARCH BY AIRLINE, returns Airline code, Airline name, number of delay records in database (includes non delayed flights), 
+-- delay rate, number of delay of flights within same state, number of delay of flights within different state
+
 DELIMITER //
-CREATE PROCEDURE GetUserDelays(IN user_id_in INT, OUT avg_delay )
+
+CREATE PROCEDURE GetFlightsDelayRatesAndStates(IN input_airline VARCHAR(2))
 BEGIN
-    DECLARE done INT DEFAULT 0;
-    DECLARE user_delay_number INT;
-    DECLARE user_flight_number INT;
-    DECLARE user_airline_code VARCHAR(2);
-    DECLARE user_minutes INT;
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE airline_code_var VARCHAR(2);
+    DECLARE airline_name_var VARCHAR(255);
+    DECLARE num_flights INT;
+    DECLARE delay_rate FLOAT;
 
-    DECLARE user_delays_cursor CURSOR FOR
-        SELECT Delay.delay_number, Delay.flight_number, Delay.airline_code, Delay.minutes
-        FROM Delay
-        WHERE Delay.user_id = user_id_in;
+	DECLARE same_state_delay INT DEFAULT 0;
+    DECLARE diff_state_delay INT DEFAULT 0;
+    DECLARE total_state_delay INT DEFAULT 0;
+    DECLARE orig_state_var VARCHAR(2);
+    DECLARE dest_state_var VARCHAR(2);
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+DECLARE results_cursor CURSOR FOR
+        SELECT orig_airport.state AS orig_state_var, dest_airport.state AS dest_state_var
+		FROM Delay AS d NATURAL JOIN Operate_Flight AS o
+		JOIN Airport AS orig_airport ON orig_airport.airport_code = o.orig_airport_code
+		JOIN Airport AS dest_airport ON dest_airport.airport_code = o.dest_airport_code
+		WHERE d.airline_code = input_airline AND d.minutes > 0;
 
-    OPEN user_delays_cursor;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-    user_delay_loop: LOOP
-        FETCH user_delays_cursor INTO user_delay_number, user_flight_number, user_airline_code, user_minutes;
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_results (
+    airline_code_var VARCHAR(2),
+    airline_name_var VARCHAR(255),
+    num_flights INT,
+    delay_rate FLOAT,
+    same_state_delay INT DEFAULT 0,
+    diff_state_delay INT DEFAULT 0,
+    total_state_delay INT DEFAULT 0
+);
 
-        IF done = 1 THEN
-            LEAVE user_delay_loop;
+INSERT INTO temp_results (airline_code_var, airline_name_var, num_flights, delay_rate)
+SELECT a.airline_code AS airline_code_var,
+    a.airline_name AS airline_name_var,
+    COUNT(d.delay_number) AS num_flights,
+    ROUND((SUM(CASE WHEN d.minutes > 0 THEN 1 ELSE 0 END) / COUNT(d.delay_number)) * 100, 2) AS delay_rate
+FROM Airline a NATURAL JOIN Delay d
+WHERE a.airline_code = input_airline
+GROUP BY a.airline_code;
+
+OPEN results_cursor;
+
+results_loop: LOOP
+        FETCH results_cursor INTO orig_state_var, dest_state_var;
+
+-- Exit if no more data
+        IF done THEN
+            LEAVE results_loop;
         END IF;
 
-        // 
+		SET total_state_delay = total_state_delay + 1;
+        IF orig_state_var = dest_state_var THEN
+            SET same_state_delay = same_state_delay + 1;
+        ELSE
+            SET diff_state_delay = diff_state_delay + 1;
+        END IF;
+
+-- Update the temporary table after processing each row
+        UPDATE temp_results
+        SET same_state_delay = same_state_delay, diff_state_delay = diff_state_delay, total_state_delay = total_state_delay;
     END LOOP;
 
-    CLOSE user_delays_cursor;
-    
+-- Close the cursor
+    CLOSE results_cursor;
+
+    SELECT * FROM temp_results;
+
+DROP TEMPORARY TABLE IF EXISTS temp_results;
 END //
-DELIMITER ;
+
+DELIMITER //
