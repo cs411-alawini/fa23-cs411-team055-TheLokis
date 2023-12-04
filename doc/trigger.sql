96c0ec100bfd2fa1b23c015e8407add956253a05
JackWangUIUC <jiamu2@illinois.edu>
2023-12-04 15:19:03 -0600

    Updated trigger

--- doc/trigger.sql
+++ doc/trigger.sql
@@ -1,3 +1,9 @@
+DELIMITER //
+
+CREATE TRIGGER BeforeInsertDelay
+BEFORE INSERT ON Delay
+FOR EACH ROW
+
 BEGIN
     DECLARE db_airline_name VARCHAR(255);
 
@@ -12,3 +18,7 @@ BEGIN
         VALUES(NEW.airline_code, "Others");
     END IF;
 END
+
+
+
+DELIMITER ;
